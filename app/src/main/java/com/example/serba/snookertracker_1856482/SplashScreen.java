7a42d93a5ae0f6dedd61454ea6037dc602bf11fa
Bogdan <serbanbogdan2008@yahoo.com>
2018-02-01 19:56:21 +0000

    created splash screen, added theme colors, dims

--- app/src/main/java/com/example/serba/snookertracker_1856482/SplashScreen.java
+++ app/src/main/java/com/example/serba/snookertracker_1856482/SplashScreen.java
@@ -1,13 +1,28 @@
 package com.example.serba.snookertracker_1856482;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MenuActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
     }
+
 }
